e64bb8b0f394aebcb62dd5fa73dcf942d0dd5a9e
jinhee <wiostz98kr@gmail.com>
2023-05-17 20:11:52 +0900

    refact: Pagecomponent와 ImageComponent가 BaseComponent 상속하도록 수정

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,16 +1,11 @@
-export class ImageComponent {
-  private element: HTMLElement;
+import { BaseComponent } from './../../component.js';
 
+export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
-    const template = document.createElement('template');
-
-    //🌟사용자로부터 받은 데이터를 html에 넣고 싶을 때 innerHTML을 사용하면 위험함. 입력값이 없을 땐 상관X
-    template.innerHTML = `<section class="image">
-    <div class="image__holder"><img class="image__thumbnail"></div>
-    <p class="image__title"></p>
-  </section>`;
-    this.element = template.content.firstElementChild! as HTMLElement;
-    //firstElementChild: read-only property returns an element's first child Element, or null if there are no child elements.
+    super(`<section class="image">
+            <div class="image__holder"><img class="image__thumbnail"></div>
+            <p class="image__title"></p>
+           </section>`);
 
     const imageElement = this.element.querySelector(
       '.image__thumbnail'
@@ -22,8 +17,5 @@ export class ImageComponent {
       '.image__title'
     )! as HTMLParagraphElement;
     titleElement.textContent = title;
-  }
-  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-    parent.insertAdjacentElement(position, this.element);
   }
 }
