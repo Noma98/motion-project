d497682f12e0084e9deda5aef7bced774448ea02
jinhee <wiostz98kr@gmail.com>
2023-06-01 19:36:01 +0900

    add: Componen의 wrapper 컴포넌트인 PageItemComponent 구현

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,11 @@ import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
+import { Component } from './components/component.js';
 
 class App {
-  private readonly page: PageComponent;
-
+  private readonly page: Component & Composable; //클래스 타입이 아니라 인터페이스 타입으로 지정하는 것이 좋음
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
@@ -15,19 +15,19 @@ class App {
       'My Image',
       'https://picsum.photos/500/300'
     );
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
 
     const note = new NoteComponent('Note Title', 'Note Body');
-    note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
 
     const todo = new TodoComponent('Todo Title', 'Todo');
-    todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
 
     const video = new VideoComponent(
       'Video Title',
       'https://youtu.be/AedcYlARH8g'
     );
-    video.attachTo(appRoot, 'beforeend');
+    this.page.addChild(video);
   }
 }
 new App(document.querySelector('.document') as HTMLElement);
